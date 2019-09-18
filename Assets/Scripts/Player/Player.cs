99992acea53707e363f3487e3e66fddaafb8f515
Brandon Cordell Jones <bcjones1@fullsail.edu>
2019-09-17 21:52:10 -0400

    Initial commit
    initial commit

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(PlayerMovementController))]
+[RequireComponent(typeof(PlayerCameraLook))]
+public class Player : Character
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
